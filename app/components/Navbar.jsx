f1c2c6159ae5d490d7f600de5b5e1280d03bdf3e
Hemanth <hemanth2102003@gmail.com>
2025-04-19 23:16:30 +0530

    Added navbar logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import NavLink from './NavLink'
 import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid'
 import MenuOverlay from './MenuOverlay'
+import Image from 'next/image';
 const navLinks=[
     { title: 'About', path: '#about' },
     { title: 'Projects', path: '#projects' },
@@ -16,7 +17,17 @@ const Navbar = () => {
     <nav className='fixed top-0 left-0 right-0 z-10' style={{ backgroundColor: 'rgba(18, 18, 18, 1)' }}>
         <div>
             <div className='flex flex-wrap items-center justify-between mx-auto p-4 py-2'>
-            <Link href="/" className='text-lg lg:text-xl md:text-xl text-white font-semibold'>LOGO</Link>
+            <Link href="/" className="text-lg lg:text-xl md:text-xl text-white font-semibold">
+  <div className="relative w-[60px] h-[60px] lg:w-[80px] lg:h-[80px]"> {/* Adjust size as needed */}
+    <Image
+      src="/images/toplogo.png"
+      alt="hero image"
+      fill
+      className="object-cover"
+    />
+  </div>
+</Link>
+
             <div className='mobile-menu block md:hidden'>{
                 !navbarOpen ? (
                     <button onClick={()=> setNavbarOpen(true)} className='flex items-center px-3 py-2   hover:text-white hover:border-white text-slate-200 '>
